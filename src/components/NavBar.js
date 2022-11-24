e1962410c622da2512b4c74da51ad7106357b490
Akbar Khan <akbarkhan061192@gmail.com>
2022-11-24 22:36:15 +0530

    navbar responsive final code

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,17 +1,23 @@
 import { useState, useEffect } from 'react';
+import logo from '../assets/logo.png';
+import React from 'react';
 import AppBar from '@mui/material/AppBar';
 import Box from '@mui/material/Box';
 import Toolbar from '@mui/material/Toolbar';
+import IconButton from '@mui/material/IconButton';
 import Typography from '@mui/material/Typography';
+import Menu from '@mui/material/Menu';
+import MenuIcon from '@mui/icons-material/Menu';
 import Container from '@mui/material/Container';
+import Avatar from '@mui/material/Avatar';
 import Button from '@mui/material/Button';
-import logo from '../assets/logo.png';
-
-const pages = ['What We Do', 'Solutions', 'Marketplace', 'Partner with Us'];
+import Tooltip from '@mui/material/Tooltip';
+import MenuItem from '@mui/material/MenuItem';
 
 const ResponsiveAppBar = () => {
   // const [activeLink, setActiveLink] = useState('');
   const [scrolled, setScrolled] = useState(false);
+  const pages = ['What We Do', 'Solutions', 'Marketplace', 'Partner with Us', 'Connect Us'];
 
   useEffect(() => {
     const onScroll = () => {
@@ -26,6 +32,24 @@ const ResponsiveAppBar = () => {
 
     return () => window.removeEventListener('scroll', onScroll);
   }, []);
+
+  const [anchorElNav, setAnchorElNav] = React.useState(null);
+  const [anchorElUser, setAnchorElUser] = React.useState(null);
+
+  const handleOpenNavMenu = event => {
+    setAnchorElNav(event.currentTarget);
+  };
+  const handleOpenUserMenu = event => {
+    setAnchorElUser(event.currentTarget);
+  };
+
+  const handleCloseNavMenu = () => {
+    setAnchorElNav(null);
+  };
+
+  const handleCloseUserMenu = () => {
+    setAnchorElUser(null);
+  };
 
   return (
     <AppBar elevation={scrolled ? 3 : 0} style={{ position: 'fixed', top: 0, background: 'white' }}>
@@ -46,18 +70,74 @@ const ResponsiveAppBar = () => {
               textDecoration: 'none',
             }}
           >
-            <img src={logo} alt='logo' width={290} />
+            <img src={logo} alt='logo' width={350} />
           </Typography>
-          {/* <div style={{ flex: 1 }}></div> */}
-          <Box style={{ display: 'flex', justifyContent: 'space-evenly' }} sx={{ flexGrow: 1, display: { xs: 'none', md: 'flex' } }}>
+
+          <Box sx={{ flexGrow: 1, display: { xs: 'flex', md: 'none' } }}>
+            <IconButton
+              size='large'
+              aria-label='account of current user'
+              aria-controls='menu-appbar'
+              aria-haspopup='true'
+              onClick={handleOpenNavMenu}
+              color='primary'
+            >
+              <MenuIcon />
+            </IconButton>
+            <Menu
+              id='menu-appbar'
+              anchorEl={anchorElNav}
+              anchorOrigin={{
+                vertical: 'bottom',
+                horizontal: 'left',
+              }}
+              keepMounted
+              transformOrigin={{
+                vertical: 'top',
+                horizontal: 'left',
+              }}
+              open={Boolean(anchorElNav)}
+              onClose={handleCloseNavMenu}
+              sx={{
+                display: { xs: 'block', md: 'none' },
+              }}
+            >
+              {pages.map(page => (
+                <MenuItem key={page} onClick={handleCloseNavMenu}>
+                  <Typography sx={{ color: `${page === 'Connect Us' ? '#f39305' : '#0b4399'} ` }} textAlign='center'>
+                    {page}
+                  </Typography>
+                </MenuItem>
+              ))}
+            </Menu>
+          </Box>
+          <Typography
+            variant='h5'
+            noWrap
+            component='a'
+            href=''
+            sx={{
+              mr: 2,
+              display: { xs: 'flex', md: 'none' },
+              fontFamily: 'monospace',
+              fontWeight: 700,
+              letterSpacing: '.3rem',
+              color: 'inherit',
+              textDecoration: 'none',
+            }}
+          >
+            <img src={logo} alt='logo' width={350} />
+          </Typography>
+          <Box sx={{ flexGrow: 1, display: { xs: 'none', md: 'flex', justifyContent: 'space-evenly' } }}>
             {pages.map(page => (
-              <Button style={{ color: '#0b4399' }} key={page} sx={{ my: 2, color: 'white', display: 'block' }}>
+              <Button
+                key={page}
+                onClick={handleCloseNavMenu}
+                sx={{ my: 2, color: `${page === 'Connect Us' ? '#f39305' : '#0b4399'} `, display: 'block' }}
+              >
                 {page}
               </Button>
             ))}
-            <Button style={{ color: '#f39305', borderRadius: 0 }} sx={{ my: 2, color: 'white', display: 'block' }}>
-              Connect Us
-            </Button>
           </Box>
         </Toolbar>
       </Container>
