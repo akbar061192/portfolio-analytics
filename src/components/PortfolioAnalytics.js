3a4abaea133cc0bb3507795e608e7e3879259168
Akbar Khan <akbarkhan061192@gmail.com>
2022-11-30 22:51:23 +0530

    portfolio analytics section

--- src/components/PortfolioAnalytics.js
+++ src/components/PortfolioAnalytics.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import { Typography } from '@mui/material';
-import portfolioAnalytics from '../assets/analytics.png';
+import portfolioAnalytics from '../assets/portfolio_analytics.jpg';
 import TripOriginRoundedIcon from '@mui/icons-material/TripOriginRounded';
 
 const PortfolioAnalytics = () => {
@@ -10,19 +10,19 @@ const PortfolioAnalytics = () => {
         style={{
           display: 'flex',
           justifyContent: 'space-evenly',
-          padding: '6rem 6rem 5rem 6rem',
+          padding: '2rem',
           border: '1px solid transparent',
-          margin: '19rem 7rem 0 7rem',
+          margin: '10rem 2rem 3rem 2rem',
           borderRadius: '20px',
           background: 'whitesmoke',
         }}
       >
-        <img src={portfolioAnalytics} alt='analyticalLogo' style={{ borderRadius: '50%' }} />
+        <img src={portfolioAnalytics} alt='analyticalLogo' width={600} style={{ borderRadius: '10px' }} />
 
         <main style={{ marginLeft: '5rem' }}>
           <Typography
             marginBottom={'1.5rem'}
-            style={{ fontSize: '3rem', fontWeight: 'bold', fontFamily: 'Poppins', lineHeight: '48px', color: '#0015FF' }}
+            style={{ fontSize: '2rem', fontWeight: 'bold', fontFamily: 'Poppins', lineHeight: '48px', color: '#0015FF' }}
           >
             Invest Wisely
           </Typography>
@@ -31,8 +31,8 @@ const PortfolioAnalytics = () => {
 
             <Typography
               marginBottom={'1rem'}
-              style={{ fontSize: '1.5rem', fontWeight: '400', fontFamily: 'Source Sans Pro', textAlign: 'justify' }}
-              color='gray'
+              style={{ fontSize: '1.2rem', fontWeight: '400', fontFamily: 'Source Sans Pro', textAlign: 'justify' }}
+              // color='gray'
             >
               With FYINNOVEA portfolio management web platform &amp; mobile apps, monitor your investments easily from anywhere and at any
               time.
@@ -43,8 +43,8 @@ const PortfolioAnalytics = () => {
             <TripOriginRoundedIcon style={{ width: '20px', marginTop: '5px', marginRight: '10px', color: '#1d12cc' }} />
             <Typography
               marginBottom={'1rem'}
-              style={{ fontSize: '1.5rem', fontWeight: '400', fontFamily: 'Source Sans Pro', textAlign: 'justify' }}
-              color='gray'
+              style={{ fontSize: '1.2rem', fontWeight: '400', fontFamily: 'Source Sans Pro', textAlign: 'justify' }}
+              // color='gray'
             >
               As the market changes, you may need to make changes in your investment goals, allocation, diversification / concentration
               &amp; timely monitoring your investments using high benchmarks &amp; decision based on data analytics
