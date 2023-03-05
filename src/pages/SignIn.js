d3cc7bba057140a9df57dc9eb0780ad5ce525fc2
Akbar Khan <akbar.khan@in.imshealth.com>
2023-03-05 20:08:53 +0530

    sign in api integration

--- src/pages/SignIn.js
+++ src/pages/SignIn.js
@@ -11,6 +11,8 @@ import Container from '@mui/material/Container';
 import { createTheme, ThemeProvider } from '@mui/material/styles';
 import { Link } from 'react-router-dom';
 import ForgotPassword from './ForgotPassword';
+import { Alert, Snackbar } from '@mui/material';
+import axios from 'axios';
 
 function Copyright(props) {
   return (
@@ -28,13 +30,51 @@ function Copyright(props) {
 const theme = createTheme();
 
 export default function SignIn() {
-  const handleSubmit = event => {
+  const [userInputs, setUserInputs] = useState({
+    email: '',
+    password: '',
+  });
+
+  const [openSnackBar, setOpenSnackBar] = useState(false);
+
+  const [snackBarMessage, setSnackBarMessage] = useState('');
+
+  const handleClose = (event, reason) => {
+    if (reason === 'clickaway') {
+      return;
+    }
+    setOpenSnackBar(false);
+  };
+
+  const handleSubmit = async event => {
     event.preventDefault();
-    const data = new FormData(event.currentTarget);
-    console.log({
-      email: data.get('email'),
-      password: data.get('password'),
-    });
+    const formInput = {
+      userid: userInputs.email,
+      password: userInputs.password,
+    };
+
+    console.log(formInput);
+
+    try {
+      const response = await axios({
+        method: 'post',
+        baseURL: 'https://35.154.127.7:9090/api/services',
+        url: '/signin',
+        data: formInput,
+      });
+      console.log(response);
+      setOpenSnackBar(true);
+      setSnackBarMessage('Successfuly signed in.');
+      setUserInputs({
+        email: '',
+        password: '',
+      });
+    } catch (error) {
+      console.log(error);
+      setOpenSnackBar(true);
+      setSnackBarMessage('Something went wrong. Please contact our support team.');
+      return error;
+    }
   };
 
   const [openForgotPassword, setOpenForgotPassword] = useState(false);
@@ -78,6 +118,15 @@ export default function SignIn() {
                 name='email'
                 autoComplete='email'
                 autoFocus
+                value={userInputs.email}
+                onChange={e =>
+                  setUserInputs(prev => {
+                    return {
+                      ...prev,
+                      email: e.target.value,
+                    };
+                  })
+                }
               />
               <TextField
                 margin='normal'
@@ -88,6 +137,15 @@ export default function SignIn() {
                 type='password'
                 id='password'
                 autoComplete='current-password'
+                value={userInputs.password}
+                onChange={e =>
+                  setUserInputs(prev => {
+                    return {
+                      ...prev,
+                      password: e.target.value,
+                    };
+                  })
+                }
               />
               <Button type='submit' fullWidth variant='contained' sx={{ mt: 3, mb: 2 }}>
                 Sign In
@@ -114,6 +172,21 @@ export default function SignIn() {
           <Copyright sx={{ mt: 8, mb: 4 }} />
         </Container>
       </ThemeProvider>
+
+      <Snackbar
+        open={openSnackBar}
+        autoHideDuration={3000}
+        onClose={handleClose}
+        anchorOrigin={{ vertical: 'bottom', horizontal: 'center' }}
+      >
+        <Alert
+          onClose={handleClose}
+          severity={snackBarMessage.includes('wrong') ? 'error' : 'success'}
+          sx={{ width: '100%' }}
+        >
+          {snackBarMessage}
+        </Alert>
+      </Snackbar>
     </>
   );
 }
